15beabcae38213fc4a50e8581eaef07a3eed583b
diasthi31 <diasthi31@gmail.com>
2024-07-24 18:01:16 -0300

    Refatoração de método para mapear Json para Objeto

--- src/main/java/br/com/alura/domain/Abrigo.java
+++ src/main/java/br/com/alura/domain/Abrigo.java
@@ -2,9 +2,15 @@ package br.com.alura.domain;
 
 public class Abrigo {
 
+    private Long id;
     private String nome;
     private String telefone;
     private String email;
+    private Pet[] pets;
+
+    public Abrigo() {
+
+    }
 
     public Abrigo(String nome, String telefone, String email) {
         this.nome = nome;
@@ -12,4 +18,43 @@ public class Abrigo {
         this.email = email;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Pet[] getPets() {
+        return pets;
+    }
+
+    public void setPets(Pet[] pets) {
+        this.pets = pets;
+    }
 }
